299de0cdfd5a3d932852953dc4caa788a7b9ec40
CHIRANJIB MOHAPATRA <77581863+chiranjib2001@users.noreply.github.com>
2024-03-12 10:26:43 +0530

    Create main.java
    run it and enjoy the game

---
+++ main.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printBoard();
+
+        while (true) {
+            playTurn(scanner);
+
+            if (checkWin() || checkTie()) {
+                printBoard();
+                break;
+            }
+
+            switchPlayer();
+            printBoard();
+        }
+
+        scanner.close();
+    }
+
+    private static void printBoard() {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("  -------");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void playTurn(Scanner scanner) {
+        int row, col;
+
+        while (true) {
+            System.out.print("Player " + currentPlayer + ", enter your move (give row number then press enter and then give column number): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+
+            if (isValidMove(row, col)) {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private static boolean checkWin() {
+        return checkRows() || checkColumns() || checkDiagonals();
+    }
+
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            System.out.println("Player " + currentPlayer + " wins!");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false; // There is an empty space, the game is not tied yet
+                }
+            }
+        }
+        System.out.println("The game is a tie!");
+        return true;
+    }
+}
